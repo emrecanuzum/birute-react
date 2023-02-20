57d8cd7e7a0cd462d492b1efa12ce2de1fa4f509
Emrecan Üzüm <uzumemrecan@gmail.com>
2023-02-21 00:22:31 +0300

    update 1.2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-import face from './assets/face.mp4';
+import './part-gpt.css';
+import face from './assets/video.webm';
 import yellowButton from './assets/yellow-button.png'
 import menuIcon from './assets/menu.png'
+import instagram from './assets/instagram.png'
+import facebook from './assets/facebook.png'
+import percent from './assets/percent.png'
+import handshake from './assets/handshake.png'
+import crm from './assets/crm.png'
+import net from './assets/net.png'
+
 function App() {
   return (
     <div className="App">
       <header className="Row-Header">
         <h1>Birutė</h1>
-        <img src={menuIcon} alt=""></img>
+        <a href='-' alt="">
+          <img src={menuIcon} alt=""></img>
+        </a>    
       </header>
 
       <div className="Landing">
@@ -16,20 +26,74 @@ function App() {
           <p>
             Dirbtinio intelekto automatizavimo darbai verslams. Mattis augue iaculis 
             feugiat in commodo nulla consectetur id nulla. Amet et erat nisi.
-          </p>
-       
-            <img src={yellowButton} onClick="this.myfunction" alt="button" className="YellowButton"/>
-         
-  
+          </p>  
+
+          <a href='-'>
+          <img src={yellowButton} onClick="this.myfunction" alt="button" className="YellowButton"/>
+          </a>
+          <div className='bottom-buttons'>
+            <div className='buttons'>
+              <a href='-'>
+                <img src={instagram} alt="instagram"></img>
+              </a>
+            </div>
+            <div className='buttons'>
+              <a href='-'>
+                <img src={facebook} alt="instagram"></img>
+              </a>
+            </div>
+            <div className='buttons'>
+              <a href='-'>
+                <img src={percent} alt="instagram"></img>
+              </a>
+            </div>
+            <div className='buttons'>
+              <a href='-'>
+                <img src={handshake} alt="instagram"></img>
+              </a>
+            </div>
+            <div className='buttons'>
+              <a href='-'>
+                <img src={crm} alt="instagram"></img>
+              </a>
+            </div>
+            <div className='buttons'>
+              <a href='-'>
+                <img src={net} alt="instagram"></img>
+              </a>
+            </div>
+          </div>
         </div>
 
         <div className="Landing-Face">
           <video src={face} alt="face" width="100%" autoPlay muted loop>
-            
           </video>
         </div>
       </div>
-      
+      {/**/}
+      <div className='part-gpt'>
+
+        <div className='buttons-left'>
+
+        </div>
+
+        <div className='input-area'>
+          <form>
+            <label>
+              <input type="text" name="name"placeholder="What's your programming question ? be specific."/>
+            </label>
+          </form>
+        </div>
+
+        <div className='output-screen'>
+
+        </div>
+
+
+        <div className='youtube-videos'>
+
+        </div>
+      </div>
     </div>
 
   );
