9f8bb9d186bd557c8a1b3d7f6b3151407d8e4a4b
Emrecan Üzüm <uzumemrecan@gmail.com>
2023-02-20 05:23:54 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import face from './assets/face.mp4';
+import yellowButton from './assets/yellow-button.png'
+import menuIcon from './assets/menu.png'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Row-Header">
+        <h1>Birutė</h1>
+        <img src={menuIcon} alt=""></img>
       </header>
+
+      <div className="Landing">
+        <div className="Landing-Title">
+          <h1>Lietuviškas dirbtinio intelekto asistentas</h1>
+          <p>
+            Dirbtinio intelekto automatizavimo darbai verslams. Mattis augue iaculis 
+            feugiat in commodo nulla consectetur id nulla. Amet et erat nisi.
+          </p>
+       
+            <img src={yellowButton} onClick="this.myfunction" alt="button" className="YellowButton"/>
+         
+  
+        </div>
+
+        <div className="Landing-Face">
+          <video src={face} alt="face" width="100%" autoPlay muted loop>
+            
+          </video>
+        </div>
+      </div>
+      
     </div>
+
   );
 }
 
